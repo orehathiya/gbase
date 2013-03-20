a6d30de208f13be41ea712f127c5a21eb2124e6f
ma12088 <ma12088@shibaura-it.ac.jp>
2013-03-21 02:59:02 +0900

    change smtp setting

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Gbase::Application.initialize!
-
-# smtp setting
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :user_name            => 'ma12088@shibaura-it.ac.jp',
-  :password             => ENV['SMTP_PASSWORD'],
-  :authentication       => 'plain',
-  :enable_starttls_auto => true  }
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,14 @@ Gbase::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # smtp
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => 'ma12088@shibaura-it.ac.jp',
+  :password             => ENV['SMTP_PASSWORD'],
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
 end
